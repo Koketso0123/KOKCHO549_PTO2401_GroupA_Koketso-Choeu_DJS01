d35718480a211d34a8031af69e3b3a4a6228ca4c
Koketso Choeu <chueukeutlwilekoketso@gmail.com>
2024-08-23 20:26:08 +0200

    Got cunfused along the way, but finally landed

--- index.js
+++ index.js
@@ -13,57 +13,32 @@ const d = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
-
-const d2 = d + (vel*time); //calcultes new distance
-const rf = fbr*time; //calculates remaining fuel
-
-const checkIfNum = (vel,acc,time) => {
-  
-   if (typeof vel !== 'number' || vel < 0) {
-     
-        throw new Error("Invalid velocity: must be a positive number (km/h).");
-    
-   }
-  
-  if(typeof acc !== 'number'){
-    
-    throw new Error("Invalid accelaration: accelaration must be a number");
-  
-  } 
-  
-  if(typeof time !== 'number'|| time < 0){
-  
-    throw new Error("Invalid time: time must be a positive number");
-  
-  }
-  
-}
+const d2 = d + vel * (time / 3600); //calcultes new distance
+const rf = fuel - fbr * time; //calculates remaining fuel
 
 // Pick up an error with how the function below is called and make it robust to such errors
 const calcNewVel = (vel, acc, time) => {
-   
-  checkIfNum(vel,acc,time);
-  
-  if(time !== 0 && acc !== 0){
-    const timeConvToSec = time*3600; //time from hours to seconds ;
-    const convAcc = acc / 3600; //converts accelaration to m/s^2
-    
-    //finds new velocity if neccesary
-    return vel + (timeConvToSec * convAcc);
-    
+  if (typeof vel !== "number" || vel < 0) {
+    throw new Error("Invalid velocity: must be a positive number (km/h).");
   }
-  return vel + (acc*time);
-  
-}
 
-const vel2 = calcNewVel(acc, vel, time); //calculates new velocity based on acceleration
+  if (typeof acc !== "number") {
+    throw new Error("Invalid accelaration: accelaration must be a number");
+  }
+
+  if (typeof time !== "number" || time < 0) {
+    throw new Error("Invalid time: time must be a positive number");
+  }
+
+  const timeConvToHours = time / 3600; //time from seconds to hours ;
+  const convAcc = acc * 12960; //converts accelaration from m/s^2 to km/h ;
+
+  //  calculates new velocity
+  return vel + convAcc * timeConvToHours;
+};
+
+const vel2 = calcNewVel(vel, acc, time); //calculates new velocity based on acceleration
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
 console.log(`Corrected Remaining Fuel: ${rf} kg`);
-
-
-
-
-
-
